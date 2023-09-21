b12fff5829247c6f1041d7d5380210669d78782b
mcnaughtonadm <mcnaughtonadm@gmail.com>
2023-09-20 20:54:03 -0700

    Linting tests based on Flake8 recommendations

--- emll/test_linlog_model.py
+++ emll/test_linlog_model.py
@@ -19,7 +19,14 @@ pytensor.config.optimizer = "fast_compile"
     name="cobra_model",
 )
 def cobra_model_fixture(request):
-    """Loads in cobra model and sets elasticity"""
+    """
+    Fixture to create a COBRA model for testing.
+
+    Returns:
+        Tuple: COBRA model, stoichiometric matrix (N), elasticity matrix (Ex & Ey),
+        and flux (v_star) vector.
+
+    """
     model, N, v_star = models[request.param]()
     Ex = create_elasticity_matrix(model)
     Ey = create_Ey_matrix(model)
@@ -28,7 +35,13 @@ def cobra_model_fixture(request):
 
 @pytest.fixture(name="linlog_least_norm")
 def linlog_least_norm_fixture(cobra_model):
-    """Method for calculating linlog least norm"""
+    """
+    Fixture for LinLogLeastNorm calculations.
+
+    Returns:
+        LinLogLeastNorm: Instance for calculating least norm.
+
+    """
     _, N, Ex, Ey, v_star = cobra_model
     ll = LinLogLeastNorm(N, Ex, Ey, v_star)
     return ll
@@ -36,6 +49,13 @@ def linlog_least_norm_fixture(cobra_model):
 
 @pytest.fixture(name="linlog_tikhonov")
 def linlog_tikhonov_fixture(cobra_model):
+    """
+    Fixture for LinLogTikhonov calculations.
+
+    Returns:
+        LinLogTikhonov Instance for calculating Tikhonov regularization.
+
+    """
     _, N, Ex, Ey, v_star = cobra_model
     ll = LinLogTikhonov(N, Ex, Ey, v_star, lambda_=1e-6)
     return ll
@@ -43,6 +63,13 @@ def linlog_tikhonov_fixture(cobra_model):
 
 @pytest.fixture(name="linlog_link")
 def linlog_link_fixture(cobra_model):
+    """
+    Fixture for LinLogLinkMatrix calculations.
+
+    Returns:
+        LinLogLinkMatrix Instance for calculating Link Matrix transformation.
+
+    """
     _, N, Ex, Ey, v_star = cobra_model
     ll = LinLogLinkMatrix(N, Ex, Ey, v_star)
     return ll
@@ -53,20 +80,39 @@ def linlog_link_fixture(cobra_model):
     name="linlog_model",
 )
 def linlog_model_fixture(request, cobra_model):
+    """
+    Fixture for accessing LinLog model fixtures.
+
+    Returns:
+        Fixture: The selected LinLog model fixture.
+
+    """
     fixture_name = request.param
     fixture = request.getfixturevalue(fixture_name)
     return fixture
+
+
+def generate_random_experiment_data(ll, n_exp=1):
+    """
+    Generate random experiment data for LinLog models.
+
+    Args:
+        ll (LinLogModel): The LinLog model.
+        n_exp (int, optional): Number of experiments. Default is 1.
+
+    Returns:
+        Tuple: Random e_hat and y_hat matrices.
+    """
+    e_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll.nr))
+    y_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll.ny))
+    return e_hat_np, y_hat_np
 
 
 def test_steady_state(linlog_model):
     """Test to ensure steady state"""
     ll = linlog_model
 
-    # Fake up some experiments
-    n_exp = 1
-
-    e_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll.nr))
-    y_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll.ny))
+    e_hat_np, y_hat_np = generate_random_experiment_data(ll)
 
     e_hat_t = at.dmatrix("en")
     e_hat_t.tag.test_value = e_hat_np
@@ -100,27 +146,37 @@ def test_control_coeff(linlog_model):
 
     cx_fd = np.zeros((ll.nm, ll.nr))
     cv_fd = np.zeros((ll.nr, ll.nr))
-    for i, ei in enumerate(es):
-        xni, vni = ll.steady_state_mat(en=ei)
+    for i, e_i in enumerate(es):
+        xni, vni = ll.steady_state_mat(en=e_i)
         cx_fd[:, i] = xni / fd
         cv_fd[:, i] = (vni - 1) / fd
 
     np.testing.assert_allclose(
-        cx_fd, ll.metabolite_control_coefficient(en=ei), atol=1e-5, rtol=1e-4
+        cx_fd, ll.metabolite_control_coefficient(en=e_i), atol=1e-5, rtol=1e-4
     )
-    np.testing.assert_allclose(cv_fd, ll.flux_control_coefficient(en=ei), atol=1e-5, rtol=1e-4)
+    np.testing.assert_allclose(cv_fd, ll.flux_control_coefficient(en=e_i), atol=1e-5, rtol=1e-4)
 
 
 def test_reduction_methods(cobra_model):
-    """Test for reduction methods"""
-    model, N, Ex, Ey, v_star = cobra_model
+    """
+    Test for LinLogLeastNorm reduction methods.
+
+    This test verifies the consistency of reduction methods in LinLogLeastNorm.
+    It creates three LinLogLeastNorm instances with different reduction methods
+    ('smallbone', 'waldherr', and None), generates random experiment data for each
+    instance, and calculates the steady state. It then compares the steady-state
+    results between the instances and checks for numerical stability.
+
+    Args:
+        cobra_model (pytest.fixture): A fixture providing a COBRA model for testing.
+
+    """
+    _, N, Ex, Ey, v_star = cobra_model
     ll1 = LinLogLeastNorm(N, Ex, Ey, v_star, reduction_method="smallbone")
     ll2 = LinLogLeastNorm(N, Ex, Ey, v_star, reduction_method="waldherr")
     ll3 = LinLogLeastNorm(N, Ex, Ey, v_star, reduction_method=None)
 
-    n_exp = 1
-    e_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll1.nr))
-    y_hat_np = 2 ** (0.5 * np.random.randn(n_exp, ll1.ny))
+    e_hat_np, y_hat_np = generate_random_experiment_data(ll1)
 
     x1, v1 = ll1.steady_state_mat(en=e_hat_np[0], yn=y_hat_np[0])
     x2, v2 = ll2.steady_state_mat(en=e_hat_np[0], yn=y_hat_np[0])
