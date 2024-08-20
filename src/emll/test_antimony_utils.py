12644c1bdd9eb328abb0dac2f381de12dee43bdb
janisshin <janiseshin@gmail.com>
2024-08-20 12:59:57 -0700

    changed filepaths and append to write

--- src/emll/test_antimony_utils.py
+++ src/emll/test_antimony_utils.py
@@ -5,13 +5,24 @@ import numpy as np
 import tellurium as te
 import cobra
 
-from emll.util import ant_to_cobra
+import logging
+logging.getLogger("cobra").setLevel(logging.ERROR)
+
+# from emll.util import ant_to_cobra
+from util import ant_to_cobra
 
 def compare_stoich_matrices(antimony_path):
+    """
+    Compares the stoichiometric matrices of original antimony file 
+    and cobra-compatible sbml file to see if they are the same.
+    
+    input: path to original antimony file (non-cobra-compatible)
+    """
     r = te.loada(antimony_path)
     N_te = r.getFullStoichiometryMatrix()
-  
-    model = cobra.io.read_sbml_model(ant_to_cobra(antimony_path) + '.xml')
+    
+    file_name = antimony_path.split('.')[0]
+    model = cobra.io.read_sbml_model(file_name + '_cobra.xml')
     N_cobra = cobra.util.create_stoichiometric_matrix(model)
 
     assert N_te.shape == N_cobra.shape
